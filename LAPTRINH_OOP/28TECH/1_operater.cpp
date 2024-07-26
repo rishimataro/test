89e1dfabca9c7ee04934b5b1c75c890bc7f2835b
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-07-26 20:40:35 +0700

    Add 1_operator.cpp

---
+++ LAPTRINH_OOP/28TECH/1_operater.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class soPhuc
+{
+    private:
+        int thuc;
+        int ao;
+    public:
+        friend istream& operator >> (istream& in, soPhuc& a);
+        friend ostream& operator << (ostream& out, soPhuc a);    
+        soPhuc operator + (soPhuc another);
+        soPhuc operator - (soPhuc another);
+};
+
+istream& operator >> (istream& in, soPhuc& a){
+    cout << "Nhap so thuc va so ao cua so phuc: ";
+    in >> a.thuc >> a.ao;
+    return in;
+}
+
+ostream& operator << (ostream& out, soPhuc a){
+    out << "So phuc: " << a.thuc << "i + " << a.ao << endl;
+    return out;
+}
+
+soPhuc soPhuc::operator + (soPhuc another){
+    soPhuc tong;
+    tong.thuc = this->thuc + another.thuc;
+    tong.ao = this->ao + another.ao;
+    return tong;
+}
+
+soPhuc soPhuc::operator - (soPhuc another){
+    soPhuc hieu;
+    hieu.thuc = this->thuc - another.thuc;
+    hieu.ao = this->ao - another.ao;
+    return hieu;
+}
+
+int main() {
+    soPhuc x, y;
+    cin >> x >> y;
+
+    soPhuc tong = x + y;
+    soPhuc hieu = x - y;
+
+    cout << tong << endl;
+    cout << hieu << endl;
+
+    return 0;
+}
